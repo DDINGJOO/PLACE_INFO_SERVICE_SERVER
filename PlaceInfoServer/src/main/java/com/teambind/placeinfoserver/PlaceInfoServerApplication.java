f7086881f256fd2b4277fba080ca7e2bea118816
ddingjoo <ddingsha9@teambind.co.kr>
2025-10-26 15:10:32 +0900

    "Initialize `PlaceInfoServer` project with Spring Boot, Gradle, and essential configurations."

---
+++ PlaceInfoServer/src/main/java/com/teambind/placeinfoserver/PlaceInfoServerApplication.java
@@ -0,0 +1,13 @@
+package com.teambind.placeinfoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceInfoServerApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceInfoServerApplication.class, args);
+	}
+	
+}
---
+++ PlaceInfoServer/src/test/java/com/teambind/placeinfoserver/PlaceInfoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teambind.placeinfoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceInfoServerApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}

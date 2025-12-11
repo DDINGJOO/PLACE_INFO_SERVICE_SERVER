6e4970393027839e2a8bb8806af96bcffa1bc9c5
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-11 10:33:38 +0900

    [STYLE] InvalidPlaceNameException 코드 포맷팅 정리

--- PlaceInfoServer/src/main/java/com/teambind/placeinfoserver/place/common/exception/domain/InvalidPlaceNameException.java
+++ PlaceInfoServer/src/main/java/com/teambind/placeinfoserver/place/common/exception/domain/InvalidPlaceNameException.java
@@ -8,29 +8,29 @@ import com.teambind.placeinfoserver.place.common.exception.PlaceException;
  * HTTP 400 Bad Request
  */
 public class InvalidPlaceNameException extends PlaceException {
-
+	
 	public InvalidPlaceNameException() {
 		super(ErrorCode.PLACE_NAME_REQUIRED);
 	}
-
+	
 	public InvalidPlaceNameException(String message) {
 		super(ErrorCode.PLACE_NAME_REQUIRED, message);
 	}
-
-	@Override
-	public String getExceptionType() {
-		return "DOMAIN";
-	}
-
+	
 	public static InvalidPlaceNameException required() {
 		return new InvalidPlaceNameException("장소 이름은 필수입니다.");
 	}
-
+	
 	public static InvalidPlaceNameException tooLong(int maxLength) {
 		return new InvalidPlaceNameException("장소 이름은 " + maxLength + "자를 초과할 수 없습니다.");
 	}
-
+	
 	public static InvalidPlaceNameException empty() {
 		return new InvalidPlaceNameException("장소 이름은 공백일 수 없습니다.");
+	}
+	
+	@Override
+	public String getExceptionType() {
+		return "DOMAIN";
 	}
 }

2d9560dbc14542bd1e18bd34dc58f3e1c139ed9f
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-11 10:33:33 +0900

    [STYLE] InvalidContactException 코드 포맷팅 정리

--- PlaceInfoServer/src/main/java/com/teambind/placeinfoserver/place/common/exception/domain/InvalidContactException.java
+++ PlaceInfoServer/src/main/java/com/teambind/placeinfoserver/place/common/exception/domain/InvalidContactException.java
@@ -8,38 +8,38 @@ import com.teambind.placeinfoserver.place.common.exception.PlaceException;
  * HTTP 400 Bad Request
  */
 public class InvalidContactException extends PlaceException {
-
+	
 	public InvalidContactException(ErrorCode errorCode) {
 		super(errorCode);
 	}
-
+	
 	public InvalidContactException(ErrorCode errorCode, String message) {
 		super(errorCode, message);
 	}
-
-	@Override
-	public String getExceptionType() {
-		return "DOMAIN";
-	}
-
+	
 	public static InvalidContactException invalidPhone(String phoneNumber) {
 		return new InvalidContactException(
 				ErrorCode.CONTACT_INVALID_PHONE,
 				"유효하지 않은 전화번호 형식입니다: " + phoneNumber
 		);
 	}
-
+	
 	public static InvalidContactException invalidEmail(String email) {
 		return new InvalidContactException(
 				ErrorCode.CONTACT_INVALID_EMAIL,
 				"유효하지 않은 이메일 형식입니다: " + email
 		);
 	}
-
+	
 	public static InvalidContactException invalidUrl(String url) {
 		return new InvalidContactException(
 				ErrorCode.CONTACT_INVALID_URL,
 				"유효하지 않은 URL 형식입니다: " + url
 		);
+	}
+	
+	@Override
+	public String getExceptionType() {
+		return "DOMAIN";
 	}
 }

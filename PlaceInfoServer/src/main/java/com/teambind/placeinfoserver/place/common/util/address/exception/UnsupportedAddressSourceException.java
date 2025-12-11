22158d3b1b9d1c4d25b0e9563090e7029653c6d0
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-11 10:34:02 +0900

    [STYLE] UnsupportedAddressSourceException 코드 포맷팅 정리

--- PlaceInfoServer/src/main/java/com/teambind/placeinfoserver/place/common/util/address/exception/UnsupportedAddressSourceException.java
+++ PlaceInfoServer/src/main/java/com/teambind/placeinfoserver/place/common/util/address/exception/UnsupportedAddressSourceException.java
@@ -6,11 +6,11 @@ import com.teambind.placeinfoserver.place.domain.enums.AddressSource;
  * 지원하지 않는 주소 소스일 때 발생하는 예외
  */
 public class UnsupportedAddressSourceException extends RuntimeException {
-
+	
 	public UnsupportedAddressSourceException(AddressSource source) {
 		super("지원하지 않는 주소 소스입니다: " + source);
 	}
-
+	
 	public UnsupportedAddressSourceException(String message) {
 		super(message);
 	}
